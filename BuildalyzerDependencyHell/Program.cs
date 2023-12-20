d6074f938c1b442a4cc0489d21b8f22436a84397
Ricardo Boss <mail@ricardoboss.de>
2023-12-20 02:28:20 +0100

    Initial commit

---
+++ BuildalyzerDependencyHell/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Buildalyzer;
+
+var manager = new AnalyzerManager();
+var analyzer = manager.GetProject(@"..\..\..\BuildalyzerDependencyHell.csproj");
+var results = analyzer.Build();
+
+foreach (var result in results)
+{
+    foreach (var sourceFile in result.SourceFiles)
+    {
+        Console.WriteLine(sourceFile);
+    }
+}
